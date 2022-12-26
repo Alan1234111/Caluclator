ec552d5a9afda7a885c9ae9a8fc2de821a0bf564
Alan Pason <alan.pason132@gmail.com>
2022-12-26 17:31:27 +0100

    Add EventListener to allButtons

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-console.log("fz");
+// reduce works on an array
+
+// if operations === *
+//   return total * number
+
+//take all buttons
+// if user click the button operation
+//     add number to an array
+
+const allButtons = document.querySelectorAll(".buttons button");
+
+const numberToOperate = [2, 5, 6];
+const operations = ["+", "*"];
+
+// numberToOperate.reduce((total, numbers) => {});
+function operate() {
+  console.log("dzial");
+}
+
+allButtons.forEach((button) => button.addEventListener("click", operate));
